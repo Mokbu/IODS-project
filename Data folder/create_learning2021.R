11d463f1e1ce711bc91b1d72b385acc5b98863f4
Mokbul Hossain <mdho@uef.fi>
2021-11-15 08:06:37 +0200

    Data wrangling

--- Data folder/create_learning2021.R
+++ Data folder/create_learning2021.R
@@ -2,6 +2,7 @@
 Mokbul Hossain
 15.11.2021
 data description file 2021
+Data wrangling
 lrn14 <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt",sep = "\t", header = TRUE)
 dim(lrn14)
 str(lrn14)
