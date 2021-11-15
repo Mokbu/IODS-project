026d9999e343bb0d13c9682b05b163b97714c942
Mokbul Hossain <mdho@uef.fi>
2021-11-15 07:41:04 +0200

    somewhere i am stuck and does not work

--- Data folder/create_learning2021.R
+++ Data folder/create_learning2021.R
@@ -1,6 +1,7 @@
-data description file 2021
+
 Mokbul Hossain
 15.11.2021
+data description file 2021
 lrn14 <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt",sep = "\t", header = TRUE)
 dim(lrn14)
 str(lrn14)
