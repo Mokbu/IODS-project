3a022b80510139a393742848e74fd80ae531067f
Mokbul Hossain <mdho@uef.fi>
2021-11-16 20:59:31 +0200

    up

--- Data folder/create_learning2021.R
+++ Data folder/create_learning2021.R
@@ -3,7 +3,7 @@ Mokbul Hossain
 15.11.2021
 data description file 2021
 Data wrangling
-learning2014r <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt",sep = "\t", header = TRUE)
+learning2014  <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt",sep = "\t", header = TRUE)
 dim(lrn14)
 str(lrn14)
 
