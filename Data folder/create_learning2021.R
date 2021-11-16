dfd08437680fdae521af0a0323460afefc8290cc
Mokbul Hossain <mdho@uef.fi>
2021-11-16 21:20:47 +0200

    trying

--- Data folder/create_learning2021.R
+++ Data folder/create_learning2021.R
@@ -7,3 +7,117 @@ learning2014  <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JY
 dim(lrn14)
 str(lrn14)
 
+#lrn14 is available
+
+# divide each number in a vector
+c(1,2,3,4,5) / 2
+
+# print the "Attitude" column vector of the lrn14 data
+lrn14$Attitude
+
+# divide each number in the column vector
+lrn14$Attitude / 10
+
+# create column "attitude" by scaling the column "Attitude"
+lrn14$attitude <- lrn14$Attitude / 10
+
+
+# lrn14 is available
+
+# Access the dplyr library
+library(dplyr)
+
+# questions related to deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30", "D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# select the columns related to deep learning and create column 'deep' by averaging
+deep_columns <- select(lrn14, one_of(deep_questions))
+lrn14$deep <- rowMeans(deep_columns)
+
+# select the columns related to surface learning and create column 'surf' by averaging
+surface_columns <- select(lrn14, one_of(surface_questions))
+lrn14$surf <- rowMeans(surface_columns)
+
+# select the columns related to strategic learning and create column 'stra' by averaging
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+lrn14$stra <- rowMeans(strategic_columns)
+
+# lrn14 is available
+
+# Access the dplyr library
+library(dplyr)
+
+# questions related to deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30", "D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# select the columns related to deep learning and create column 'deep' by averaging
+deep_columns <- select(lrn14, one_of(deep_questions))
+lrn14$deep <- rowMeans(deep_columns)
+
+# select the columns related to surface learning and create column 'surf' by averaging
+surface_columns <- select(lrn14, one_of(surface_questions))
+lrn14$surf <- rowMeans(surface_columns)
+
+# select the columns related to strategic learning and create column 'stra' by averaging
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+lrn14$stra <- rowMeans(strategic_columns)
+
+# see the structure of the new dataset
+str(learning2014)
+str(learning2014)
+
+# learning2014 is available
+
+# print out the column names of the data
+colnames(learning2014)
+
+# change the name of the second column
+colnames(learning2014)[2] <- "age"
+
+# change the name of "Points" to "points"
+colnames(learning2014)[7] <- "points"
+
+# print out the new column names of the data
+colnames(learning2014)
+
+# access the dplyr library
+library(dplyr)
+
+# select male students
+male_students <- filter(learning2014, gender == "M")
+
+# select rows where Points is greater than zero
+learning2014 <- filter(learning2014, points > 0)
+
+
+# learning2014 is available
+
+# Access the gglot2 library
+library(ggplot2)
+
+# initialize plot with data and aesthetic mapping
+p1 <- ggplot(learning2014, aes(x = attitude, y = points))
+
+# define the visualization type (points)
+p2 <- p1 + geom_point()
+
+# draw the plot
+p2
+
+# add a regression line
+p3 <- p2 + geom_smooth(method = "lm")
+pairs(learning2014[-1], col = learning2014$gender)
+library(GGally)
+library(ggplot2)
+p <- ggpairs(learning2014, mapping = aes(col = gender, alpha = 0.3), lower = list(combo = wrap("facethist", bins = 20)))
+p
+
+
+library(ggplot2)
+qplot(attitude, points, data = learning2014) + geom_smooth(method = "lm")
+my_model <- lm(points ~ attitude, data = learning2014)
+summary(my_model)
